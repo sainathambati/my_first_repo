ee1ec3b7f13a48f4cd90f08261a0807ea3f06f46
sainathambati <ambatisainath1991@gmail.com>
2026-04-14 12:01:40 +0530

    basic_python

---
+++ python_basics_sainath.py
@@ -0,0 +1,13 @@
+name="sainath"
+age=35
+height=1.8
+is_student=True
+print(f"Name: {name} | age: {age} | Height: {height} | Student: {is_student}")
+age_in_months=age*12
+age_in_days=age*365
+remainder=age/7
+age_2power=35**2
+print(f"age_in_months: {age_in_months}")
+print(f"age_in_days: {age_in_days}")
+print(f"reaminder: {remainder}")
+print(f"2 power of age: {age_2power}")
